3ef65e62c047dc4bd77e1fe1dbb84e96c3cb03b2
BROWN-PROGRAMTECH <aldenovpoutine99@gmail.com>
2025-01-23 15:36:58 +0100

    setting riverpod state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:learningflutterappwrite/dependencies.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   initDependencies();
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
