53b9a563f9aa8a5e6e35f16aaa569a54e42521bd
BROWN-PROGRAMTECH <aldenovpoutine99@gmail.com>
2025-01-26 16:11:01 +0100

    troubleshut setting. json and env.dart

---
+++ lib/env.dart
@@ -0,0 +1,7 @@
+class EnvironmentVar {
+  String endpoint;
+  String databaseId;
+  EnvironmentVar(
+      {this.endpoint = 'https://cloud.appwrite.io/v1',
+      this.databaseId = "6791fbb7002a368fa03b"});
+}
