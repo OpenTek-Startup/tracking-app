02a9afd0ae4f36294493e6416c3301e804a7c2b1
BROWN-PROGRAMTECH <aldenovpoutine99@gmail.com>
2025-01-31 15:02:30 +0100

    basic home page structure

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learningflutterappwrite/widgets/ReusableContainer/greyContainer.dart';
 import 'package:learningflutterappwrite/widgets/drawer/drawer.dart';
 
 class Homepage extends StatefulWidget {
@@ -14,7 +15,9 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.greenAccent,
+        backgroundColor: Colors.white,
+        elevation: 4,
+        title: Center(child: Text('MediApp')),
       ),
       drawer: DrawerMenu(),
       backgroundColor: Colors.white,
@@ -22,34 +25,41 @@ class _HomepageState extends State<Homepage> {
           child: SingleChildScrollView(
         child: Column(
           children: [
-            // Row(
-            //   children: [TextButton(onPressed: () {}, child: Text('Folder'))],
-            // ),
-            // Greycontainer(
-            //     child: StreamBuilder(
-            //         stream: appProvider.folderStream,
-            //         builder: (context, snapshot) {
-            //           if (snapshot.connectionState == ConnectionState.waiting) {
-            //             return Center(child: CircularProgressIndicator());
-            //           }
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: [TextButton(onPressed: () {}, child: Text('Folder'))],
+            ),
+            Greycontainer(
+              child: Center(
+                child: Text('Home'),
+              ),
+              //     child: StreamBuilder(
+              //         stream: appProvider.folderStream,
+              //         builder: (context, snapshot) {
+              //           if (snapshot.connectionState == ConnectionState.waiting) {
+              //             return Center(child: CircularProgressIndicator());
+              //           }
 
-            //           if (snapshot.hasError) {
-            //             return Center(
-            //               child: Text(snapshot.error.toString()),
-            //             );
-            //           }
+              //           if (snapshot.hasError) {
+              //             return Center(
+              //               child: Text(snapshot.error.toString()),
+              //             );
+              //           }
 
-            //           final folders = snapshot.data ?? [];
-            //           return ListView.builder(
-            //               itemCount: folders.length,
-            //               itemBuilder: (context, index) {
-            //                 final folder = folders[index];
-            //                 return ListTile(
-            //                   title: Text(folder['title']),
-            //                   subtitle: Text(folder['description']),
-            //                 );
-            //               });
-            //         }))
+              //           final folders = snapshot.data ?? [];
+              //           return ListView.builder(
+              //               itemCount: folders.length,
+              //               itemBuilder: (context, index) {
+              //                 final folder = folders[index];
+              //                 return ListTile(
+              //                   title: Text(folder['title']),
+              //                   subtitle: Text(folder['description']),
+              //                 );
+              //               });
+              //         })
+            )
           ],
         ),
       )),
